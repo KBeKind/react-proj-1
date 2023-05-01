454b1cf126894f66d75d2ffd97a04eb191c9ebb8
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-05-01 09:42:17 -0500

    added more list functionality

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 function ListGroup() {
+  let items = ["computer", "laser", "hyperdrive", "kitty", "food"];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>Nothing Here</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
