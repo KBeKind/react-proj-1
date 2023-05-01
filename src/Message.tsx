c6463ee7ecd48d001a95f4f41e72ac0d18c4667e
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-05-01 08:17:25 -0500

    started react proj and added bootstrap cleared stuff and started building

---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+
+// vv Pascal Case vv
+function Message(){
+    const name = "Klee";
+    // vv JSX: JavaScript XML vv
+
+    if(name){
+        return <h1>Hello {name}</h1>;
+    }
+    return <h1>Hello Dude</h1>;
+}
+
+export default Message;
